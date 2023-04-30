8fad4394ebd07aec4aa35f46b66293a2c68bc963
iamsourav37 <sourav.g29@outlook.com>
2023-04-30 12:56:04 +0530

    Add project files.

---
+++ Bloggie.Web/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Bloggie.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Bloggie.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            this._logger.Log(LogLevel.Information, "this is a .net 7 project for learning.");
+            this._logger.Log(LogLevel.Warning, "Complete the project by today.");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
